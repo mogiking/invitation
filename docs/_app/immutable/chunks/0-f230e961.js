41b09622d34b300c3d198fd05195901b0be6f23d
jhsong <jinhsong@isaac-eng.com>
2023-01-02 18:01:22 +0900

    Add main image

---
+++ docs/_app/immutable/chunks/0-f230e961.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-2e7eb586.js";export{t as component};
---
+++ docs/_app/immutable/components/pages/_page.svelte-1aa92c2e.js
@@ -0,0 +1,25 @@
+import{S as de,i as ge,s as ke,k as _,a as y,q as z,I as Ee,l as f,h as a,c as P,m as d,r as J,n as r,J as pe,p as ie,D as c,b as E,K,B as x,L as ue,M as Ie,N as ve}from"../../chunks/index-c95f05a2.js";function _e(n,s,o){const l=n.slice();return l[12]=s[o],l[14]=o,l}function fe(n,s,o){const l=n.slice();return l[15]=s[o],l}function me(n){let s,o;return{c(){s=_("img"),this.h()},l(l){s=f(l,"IMG",{src:!0,alt:!0,class:!0}),this.h()},h(){pe(s.src,o=n[15])||r(s,"src",o),r(s,"alt",""),r(s,"class","svelte-1kjv7om")},m(l,m){E(l,s,m)},p:x,d(l){l&&a(s)}}}function he(n){let s,o,l,m;function b(){return n[8](n[14])}return{c(){s=_("button"),this.h()},l(h){s=f(h,"BUTTON",{class:!0}),d(s).forEach(a),this.h()},h(){r(s,"class",o=ve(n[0]===n[14]?"active":"")+" svelte-1kjv7om")},m(h,p){E(h,s,p),l||(m=K(s,"click",b),l=!0)},p(h,p){n=h,p&1&&o!==(o=ve(n[0]===n[14]?"active":"")+" svelte-1kjv7om")&&r(s,"class",o)},d(h){h&&a(s),l=!1,m()}}}function be(n){let s,o,l,m,b,h,p,D,C,k,G,H,j,L,M,R,O,g,I,Q,N,S,W,X,B,Y,Z,V,$,ee,U=n[2],u=[];for(let e=0;e<U.length;e+=1)u[e]=me(fe(n,U,e));let w=n[2],v=[];for(let e=0;e<w.length;e+=1)v[e]=he(_e(n,w,e));return{c(){s=_("meta"),o=y(),l=_("section"),m=_("h1"),b=z("결혼 합니다."),h=y(),p=_("h2"),D=z("2023년 4월 9일"),C=y(),k=_("img"),H=y(),j=_("section"),L=_("pre"),M=z(`저희,
+결혼합니다.
+
+작은 인연으로 연인이 되고,
+영혼의 반쪽으로 함께 하려 합니다.
+
+귀한 시간 내주시어
+
+저희의 시작을 축복해주세요.
+
+송재석·박미영 장남 송진호
+윤정우·정혜림 차녀 이아름
+	`),R=y(),O=_("main"),g=_("div"),I=_("div");for(let e=0;e<u.length;e+=1)u[e].c();Q=y(),N=_("div"),S=_("button"),W=z("Prev"),X=y(),B=_("button"),Y=z("Next"),Z=y(),V=_("div");for(let e=0;e<v.length;e+=1)v[e].c();this.h()},l(e){const i=Ee("svelte-19nvizw",document.head);s=f(i,"META",{name:!0,content:!0}),i.forEach(a),o=P(e),l=f(e,"SECTION",{class:!0});var t=d(l);m=f(t,"H1",{class:!0});var T=d(m);b=J(T,"결혼 합니다."),T.forEach(a),h=P(t),p=f(t,"H2",{});var te=d(p);D=J(te,"2023년 4월 9일"),te.forEach(a),t.forEach(a),C=P(e),k=f(e,"IMG",{src:!0,alt:!0}),H=P(e),j=f(e,"SECTION",{class:!0});var le=d(j);L=f(le,"PRE",{});var se=d(L);M=J(se,`저희,
+결혼합니다.
+
+작은 인연으로 연인이 되고,
+영혼의 반쪽으로 함께 하려 합니다.
+
+귀한 시간 내주시어
+
+저희의 시작을 축복해주세요.
+
+송재석·박미영 장남 송진호
+윤정우·정혜림 차녀 이아름
+	`),se.forEach(a),le.forEach(a),R=P(e),O=f(e,"MAIN",{class:!0});var ne=d(O);g=f(ne,"DIV",{class:!0});var A=d(g);I=f(A,"DIV",{class:!0,style:!0});var ae=d(I);for(let q=0;q<u.length;q+=1)u[q].l(ae);ae.forEach(a),Q=P(A),N=f(A,"DIV",{class:!0});var F=d(N);S=f(F,"BUTTON",{class:!0});var oe=d(S);W=J(oe,"Prev"),oe.forEach(a),X=P(F),B=f(F,"BUTTON",{class:!0});var ce=d(B);Y=J(ce,"Next"),ce.forEach(a),F.forEach(a),Z=P(A),V=f(A,"DIV",{class:!0});var re=d(V);for(let q=0;q<v.length;q+=1)v[q].l(re);re.forEach(a),A.forEach(a),ne.forEach(a),this.h()},h(){document.title="송진호 이아름 청첩장",r(s,"name","description"),r(s,"content","송진호 이아름 결혼식에 초대합니다."),r(m,"class","svelte-1kjv7om"),r(l,"class","svelte-1kjv7om"),pe(k.src,G=je)||r(k,"src",G),r(k,"alt",""),r(j,"class","svelte-1kjv7om"),r(I,"class","slider svelte-1kjv7om"),ie(I,"left","-"+n[1]+"%"),r(S,"class","prev svelte-1kjv7om"),r(B,"class","next svelte-1kjv7om"),r(N,"class","arrow svelte-1kjv7om"),r(V,"class","papagination svelte-1kjv7om"),r(g,"class","container svelte-1kjv7om"),r(O,"class","svelte-1kjv7om")},m(e,i){c(document.head,s),E(e,o,i),E(e,l,i),c(l,m),c(m,b),c(l,h),c(l,p),c(p,D),E(e,C,i),E(e,k,i),E(e,H,i),E(e,j,i),c(j,L),c(L,M),E(e,R,i),E(e,O,i),c(O,g),c(g,I);for(let t=0;t<u.length;t+=1)u[t].m(I,null);c(g,Q),c(g,N),c(N,S),c(S,W),c(N,X),c(N,B),c(B,Y),c(g,Z),c(g,V);for(let t=0;t<v.length;t+=1)v[t].m(V,null);$||(ee=[K(S,"click",n[4]),K(B,"click",n[3]),K(g,"mouseover",n[7]),K(g,"mouseleave",n[6])],$=!0)},p(e,[i]){if(i&4){U=e[2];let t;for(t=0;t<U.length;t+=1){const T=fe(e,U,t);u[t]?u[t].p(T,i):(u[t]=me(T),u[t].c(),u[t].m(I,null))}for(;t<u.length;t+=1)u[t].d(1);u.length=U.length}if(i&2&&ie(I,"left","-"+e[1]+"%"),i&33){w=e[2];let t;for(t=0;t<w.length;t+=1){const T=_e(e,w,t);v[t]?v[t].p(T,i):(v[t]=he(T),v[t].c(),v[t].m(V,null))}for(;t<v.length;t+=1)v[t].d(1);v.length=w.length}},i:x,o:x,d(e){a(s),e&&a(o),e&&a(l),e&&a(C),e&&a(k),e&&a(H),e&&a(j),e&&a(R),e&&a(O),ue(u,e),ue(v,e),$=!1,Ie(ee)}}}const je="./images/main.png";function Ne(n,s,o){let l=0;const m=["./images/1.png","./images/2.png","./images/3.png","./images/4.png","./images/5.png"],b=m.length;let h=0;const p=()=>{o(1,h=l*100)},D=()=>{o(0,l=l===b-1?0:l+1),p()},C=()=>{o(0,l=l===0?b-1:l-1),p()},k=M=>{o(0,l=M),p()};let G=setInterval(D,2e3);return[l,h,m,D,C,k,()=>{G=setInterval(D,2e3)},()=>{clearInterval(G)},M=>k(M)]}class ye extends de{constructor(s){super(),ge(this,s,Ne,be,ke,{})}}export{ye as default};
